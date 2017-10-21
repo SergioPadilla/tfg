b110aa9d770e39f1bd47292f539accd4ed4f7cb9
sergiopadillalp <splsergiopadilla@gmail.com>
2017-10-21 15:00:52 +0200

    first version of feq function

--- scripts/store_javascript_functions.py
+++ scripts/store_javascript_functions.py
@@ -7,26 +7,55 @@ from argparse import ArgumentParser
 from pymongo import MongoClient
 
 FEQ = """
-    function(property, lista) {
-        if (Object.prototype.toString.call(property) == '[object Object]' ||
-            Object.prototype.toString.call(lista) == '[object Object]')
-            throw 'Object is not allowed'
+    function(property, value) {
+        property = trapezoid(property);
+        value = trapezoid(value);
+        total = 0;
 
-        if (Object.prototype.toString.call(property) != Object.prototype.toString.call(lista))
-            throw 'Incompatible types'
+        if (property[2] >= value[1] && property[1] <= value[2])
+            total = 1;
 
-        if (Object.prototype.toString.call(lista) === '[object String]')
-            return property == lista
+        else if (property[3] <= value[0] || property[0] >= value[3])
+            total = 0;
 
-        else if (Object.prototype.toString.call(lista) === '[object Array]') {
-            if (lista.length > 1)
-                throw 'Not supported yet, the second argument must be a crisp'
+        else if (property[3] > value[0] || property[2] < value[1])
+            total = (property[3]-value[0]) / ((value[1]-value[0]) - (property[2]-property[3]));
 
-            if (property.length == 1)
-                return property[0] == lista
+        else
+            total = (value[3]-property[0]) / ((property[1]-property[0]) - (value[2]-value[3]));
 
-            else if (property.length == 2)
-                return property[0] <= lista && property[property.length - 1] >= lista
+        return total
+    }
+"""
+
+TO_TRAPEZOID = """
+    function(x) {
+        if (Object.prototype.toString.call(x) !== '[object Array]' &&
+            Object.prototype.toString.call(x) !== '[object Number]')
+            throw 'Incompatible type';
+
+        if (Object.prototype.toString.call(x) === '[object Array]') {
+            for (var i = 0; i < x.length; i++) {
+                if (Object.prototype.toString.call(x[i]) !== '[object Number]')
+                        throw 'Incompatible type';
+            }
+        }
+
+        var v = x;
+        if (Object.prototype.toString.call(x) === '[object Number]')
+            v = [x];
+
+        switch(v.length) {
+            case 1:
+                return [v[0], v[0], v[0], v[0]];
+            case 2:
+                return [v[0], v[0], v[1], v[1]];
+            case 3:
+                return [v[0], v[1], v[1], v[2]];
+            case 4:
+                return v.sort();
+            default:
+                throw 'Length must be smaller than 4';
         }
     }
 """
@@ -36,6 +65,7 @@ def store_functions(database):
     """Add elements in database."""
     system = database.system_js
     system.feq = FEQ
+    system.trapezoid = TO_TRAPEZOID
 
     print("JavaScript functions stored in database")
 
---
+++ test/test_to_trapezoid.py
@@ -0,0 +1,42 @@
+"""
+Created at 15/10/17
+__author__ = 'Sergio Padilla'
+"""
+import pytest
+from pymongo import MongoClient
+from scripts.store_javascript_functions import store_functions
+from pymongo.errors import OperationFailure
+
+
+@pytest.fixture
+def collection():
+    """Mongo db collection for testing."""
+    mongo = MongoClient('localhost')
+    database = mongo.test
+    store_functions(database)
+    return database['fuzzy']
+
+
+@pytest.mark.parametrize('value', (
+    {'k': 'v'},
+    [1, "some string"],
+    [{'k': 'v'}, "something"]
+))
+def test_incompatible_types(value, collection):
+    """Test exceptions."""
+    query = {'$where': 'trapezoid({})'.format(value)}
+    print(query)
+
+    #with pytest.raises(OperationFailure, message='Incompatible types'):
+    response = collection.find_one(query)
+    print(response)
+    assert False
+
+
+def test_incompatible_types_with_string(collection):
+    """Test exceptions."""
+    query = {'$where': 'trapezoid("some string")'}
+    print(query)
+
+    #with pytest.raises(OperationFailure, message='Incompatible types'):
+    collection.find_one(query)
