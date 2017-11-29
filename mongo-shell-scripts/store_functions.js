ffcff770b3d0989d43addc16f31112dfcc563220
sergiopadillalp <splsergiopadilla@gmail.com>
2017-11-29 14:02:39 +0100

    fix functions

--- mongo-shell-scripts/store_functions.js
+++ mongo-shell-scripts/store_functions.js
@@ -36,6 +36,18 @@ var trapezoid = function(x) {
     }
 };
 
+var evaluate = function (total, min) {
+    if (min !== null) {
+        if (min === 0)
+            return total > min;
+
+        else
+            return total >= min;
+    }
+    else
+        return total
+};
+
 var feq = function(property, value, min=null) {
     property = trapezoid(property);
     value = trapezoid(value);
@@ -53,127 +65,174 @@ var feq = function(property, value, min=null) {
     else
         total = (value[3]-property[0]) / ((property[1]-property[0]) - (value[2]-value[3]));
 
-    if (min !== null)
-        return total >= min;
-
-    else
-        return total
+    return evaluate(total, min)
 };
 
-var nfeq = function (property, value) {
+var nfeq = function (property, value, min=null) {
     property = trapezoid(property);
     value = trapezoid(value);
+    var total = 0;
     
-    if ((property[1] <= value[0] && property[0] !== value[1]) || (property[2] >= value[3] && property[3] !== value[2]))  // No solapa en nucleo del primero no tienen pendiente inf.
-        return 0;
+    if ((property[1] <= value[0] && property[0] !== value[1]) || (property[2] >= value[3] && property[3] !== value[2]))
+        total = 0;
 
-	else if (property[0] < value[1]) // No tienen pediente infinita en la izquierda
-
-        if (property[3] > value[2])  // Es p||sible calcular por la derecha
-            return Math.min(
-                (property[1] - value[0]) / ((value[1] - value[0]) - (property[0] - property[1])), // Por la izquierda
-                (property[2] - value[3]) / ((value[2] - value[3]) - (property[3] - property[2]))  // Por la derecha
+	else if (property[0] < value[1])
+        if (property[3] > value[2])
+            total = Math.min(
+                (property[1] - value[0]) / ((value[1] - value[0]) - (property[0] - property[1])),
+                (property[2] - value[3]) / ((value[2] - value[3]) - (property[3] - property[2]))
             );
 
-        else  // Solo se puede calcular por la izquierda
-            return (property[1] - value[0]) / ((value[1] - value[0]) - (property[0] - property[1]));
+        else
+            total = (property[1] - value[0]) / ((value[1] - value[0]) - (property[0] - property[1]));
 
-    else if (property[3] > value[2])  // No tienen pediente infinita en la derecha
-        return (property[2] - value[3]) / ((value[2] - value[3]) - (property[3] - property[2]));  // Calculo por la derecha
+    else if (property[3] > value[2])
+        total = (property[2] - value[3]) / ((value[2] - value[3]) - (property[3] - property[2]));
 
     else
-        return 1;  // Tienen pediente infinita por ambos lados (intervalos)
+        total = 1;
+    
+    return evaluate(total, min)
 };
 
-var fgt = function (property, value) {
-    if (property[2] >= value[3])  // Se solapan los nucleos de las funciones
-        return 1;
+var fgt = function (property, value, min=null) {
+    property = trapezoid(property);
+    value = trapezoid(value);
+    var total = 0;
 
-    else if (property[3] <= value[2])  // No se solapan los nucleos
-        return 0;
+    if (property[2] >= value[3])
+        total = 1;
+
+    else if (property[3] <= value[2])
+        total = 0;
 
     else
-        return (property[3] - value[2]) / ((value[3] - value[2]) - (property[2] - property[3]))  // TODO: round 2
+        total = (property[3] - value[2]) / ((value[3] - value[2]) - (property[2] - property[3]));  // TODO: round 2
+
+    return evaluate(total, min)
 };
 
-var nfgt = function (property, value) {
-    if (property[0] >= value[3]) 	// Se solapan los nucleos de las funciones
-        return 1;
+var nfgt = function (property, value, min=null) {
+    property = trapezoid(property);
+    value = trapezoid(value);
+    var total = 0;
 
-    else if (property[1] <= value[2])  // No se solapan los nucleos
-        return 0;
+    if (property[0] >= value[3])
+        total = 1;
+
+    else if (property[1] <= value[2])
+        total = 0;
 
     else
-        return (property[1] - value[2]) / ((value[3] - value[2]) - (property[0] - property[1]))  // TODO: round 2
+        total = (property[1] - value[2]) / ((value[3] - value[2]) - (property[0] - property[1]));  // TODO: round 2
+
+    return evaluate(total, min)
 };
 
-var flt = function (property, value) {
-    if (property[1] <= value[0]) 	// Se solapan los nucleos de las funciones
-        return 1;
+var flt = function (property, value, min=null) {
+    property = trapezoid(property);
+    value = trapezoid(value);
+    var total = 0;
 
-    else if (property[0] >= value[1])  // No se solapan los nucleos
-        return 0;
+    if (property[1] <= value[0])
+        total = 1;
+
+    else if (property[0] >= value[1])
+        total = 0;
 
     else
-        return (property[0] - value[1]) / ((value[0] - value[1]) - (property[1] - property[0]))  // TODO: round 2
+        total = (property[0] - value[1]) / ((value[0] - value[1]) - (property[1] - property[0]));  // TODO: round 2
+
+    return evaluate(total, min)
 };
 
-var nflt = function (property, value) {
-    if (property[3] <= value[0])  // Se solapan los nucleos de las funciones
-        return 1;
+var nflt = function (property, value, min=null) {
+    property = trapezoid(property);
+    value = trapezoid(value);
+    var total = 0;
 
-    else if (property[2]>=value[1])  // No se solapan los nucleos
-        return 0;
+    if (property[3] <= value[0])
+        total = 1;
+
+    else if (property[2]>=value[1])
+        total = 0;
 
     else
-        return (property[2] - value[1]) / ((value[0] - value[1]) - (property[3] - property[2]))  // TODO: round 2
+        total = (property[2] - value[1]) / ((value[0] - value[1]) - (property[3] - property[2]));  // TODO: round 2
+
+    return evaluate(total, min)
 };
 
-var fgte = function (property, value) {
-    if (property[2] >= value[1])  // Se solapan los nucleos de las funciones
-        return 1;
+var fgte = function (property, value, min=null) {
+    property = trapezoid(property);
+    value = trapezoid(value);
+    var total = 0;
 
-    else if (property[3] <= value[0])  // No se solapan los nucleos
-        return 0;
+    if (property[2] >= value[1])
+        total = 1;
+
+    else if (property[3] <= value[0])
+        total = 0;
 
     else
-        return (property[3] - value[0]) / ((value[1] - value[0]) - (property[2] - property[3]))  // TODO: round 2
+        total = (property[3] - value[0]) / ((value[1] - value[0]) - (property[2] - property[3]));  // TODO: round 2
+
+    return evaluate(total, min)
 };
 
-var nfgte = function (property, value) {
-    if (property[0] >= value[1])  // Se solapan los nucleos de las funciones
-        return 1;
+var nfgte = function (property, value, min=null) {
+    property = trapezoid(property);
+    value = trapezoid(value);
+    var total = 0;
 
-    else if (property[1] <= value[0])  // No se solapan los nucleos
-        return 0;
+    if (property[0] >= value[1])
+        total = 1;
+
+    else if (property[1] <= value[0])
+        total = 0;
 
     else
-        return (property[1] - value[0]) / ((value[1] - value[0]) - (property[0] - property[1]))  // TODO: round 2
+        total = (property[1] - value[0]) / ((value[1] - value[0]) - (property[0] - property[1]));  // TODO: round 2
+
+    return evaluate(total, min)
 };
 
-var flte = function (property, value) {
-    if (property[1] <= value[2]) 	// Se solapan los nucleos de las funciones
-        return 1;
+var flte = function (property, value, min=null) {
+    property = trapezoid(property);
+    value = trapezoid(value);
+    var total = 0;
 
-    else if (property[0] >= value[3])  // No se solapan los nucleos
-        return 0;
+    if (property[1] <= value[2])
+        total = 1;
+
+    else if (property[0] >= value[3])
+        total = 0;
 
     else
-        return (value[3] - property[0]) / ((property[1] - property[0]) - (value[2] - value[3]))  // TODO: round 2
+        total = (value[3] - property[0]) / ((property[1] - property[0]) - (value[2] - value[3])); // TODO: round 2
+
+    return evaluate(total, min)
 };
 
-var nflte = function (property, value) {
-    if (property[3] <= value[2]) 	// Se solapan los nucleos de las funciones
-        return 1;
+var nflte = function (property, value, min=null) {
+    property = trapezoid(property);
+    value = trapezoid(value);
+    var total = 0;
 
-    else if (property[2] >= value[3])  // No se solapan los nucleos
-        return 0;
+    if (property[3] <= value[2])
+        total = 1;
+
+    else if (property[2] >= value[3])
+        total = 0;
 
     else
-        return (value[3] - property[2]) / ((property[3] - property[2]) - (value[2] - value[3]))  // TODO: round 2
+        total = (value[3] - property[2]) / ((property[3] - property[2]) - (value[2] - value[3])); // TODO: round 2
+
+    return evaluate(total, min)
 };
 
 db.system.js.save({_id: 'trapezoid', value: trapezoid});
+db.system.js.save({_id: 'evaluate', value: evaluate});
 db.system.js.save({_id: 'feq', value: feq});
 db.system.js.save({_id: 'nfeq', value: nfeq});
 db.system.js.save({_id: 'fgt', value: fgt});
@@ -185,4 +244,4 @@ db.system.js.save({_id: 'nfgte', value: nfgte});
 db.system.js.save({_id: 'flte', value: flte});
 db.system.js.save({_id: 'nflte', value: nflte});
 
-print('Funciones "trapezoid" y "feq" almacenadas c||rectamente.');
+print('Funciones almacenadas corectamente.');
